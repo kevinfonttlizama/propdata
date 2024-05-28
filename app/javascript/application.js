5a9e16a825e6d2d8c7206215a8f01c393e8ad60f
kevinfonttlizama <kevin.andresfontt@gmail.com>
2024-05-28 01:01:18 -0400

    login devise fix and config

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,15 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
-import "bootstrap"
-import "@popperjs/core"
+import Rails from "@rails/ujs";
+import * as ActiveStorage from "@rails/activestorage";
+import "@hotwired/turbo-rails";
+import "controllers";
+import "bootstrap";
+import "@popperjs/core";
+
+console.log("Rails UJS:", Rails);
+console.log("ActiveStorage:", ActiveStorage);
+console.log("Turbo:", import("@hotwired/turbo-rails"));
+console.log("Bootstrap:", import("bootstrap"));
+console.log("Popper:", import("@popperjs/core"));
+
+Rails.start();
+ActiveStorage.start();
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,9 +1,9 @@
-# Pin npm packages by running ./bin/importmap
-
-pin "application"
-pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "@hotwired/stimulus", to: "stimulus.min.js"
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin "application", preload: true
+pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.0.4/lib/assets/compiled/rails-ujs.js"
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+pin "@popperjs/core", to: "https://unpkg.com/@popperjs/core@2.11.6/dist/umd/popper.min.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.3.0/dist/js/bootstrap.esm.js"
-pin "@popperjs/core", to: "https://unpkg.com/@popperjs/core@2.11.6/dist/esm/index.js"
+pin "@rails/activestorage", to: "https://ga.jspm.io/npm:@rails/activestorage@7.0.4/lib/assets/compiled/activestorage.js"
