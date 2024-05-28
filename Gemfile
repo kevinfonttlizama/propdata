54a89fa399df529d7a126ba319bfa8087a656181
kevinfonttlizama <kevin.andresfontt@gmail.com>
2024-05-27 20:59:15 -0400

    añadiendo instancias de property

--- Gemfile
+++ Gemfile
@@ -12,6 +12,9 @@ gem "dotenv-rails", groups: [:development, :test]
 
 gem "httparty"
 
+gem 'image_processing', '~> 1.2'
+
+
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.1.3", ">= 7.1.3.2"
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,31 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# property = Property.new(
+#   property_type: "Venta",
+#   price: 1500000,
+#   currency: "USD",
+#   commune: "Santiago",
+#   address: "camino el chin 124",
+#   area: 120,
+#   bedrooms: 3,
+#   bathrooms: 2,
+#   latitude: -33.4489,
+#   longitude: -70.6693,
+#   description: "Una hermosa propiedad ubicada en el corazon de la ciudad"
+# )
+
+# # Adjuntar una foto
+# property.photos.attach(
+#   io: File.open('C:\Users\Victus\OneDrive\Escritorio\dataprop\propdata\app\assets\images\casa_1.jpg'),
+#   filename: 'casa_1.jpg',
+#   content_type: 'image/jpg'
+# )
+
+# if property.save
+#   puts "Property saved successfully!"
+# else
+#   puts "Failed to save property:"
+#   puts property.errors.full_messages
+# end
