05e78704b82f5ccca5adfd1e9c72fbfb75fc3a31
kevinfonttlizama <kevin.andresfontt@gmail.com>
2024-05-28 21:20:28 -0400

    confguracion rutas

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
+  get 'dashboard/index'
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
 
   root 'home#index'
 
   get 'home', to: 'home#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'dashboard', to: 'dashboard#index'
 
   get "up" => "rails/health#show", as: :rails_health_check
+
+  resources :properties, only: [:show, :new, :create, :edit, :update, :destroy]
 end
