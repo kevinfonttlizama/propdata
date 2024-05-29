52624b79a5f6e0287b6c274a29bc73c0cd62b00f
kevinfonttlizama <kevin.andresfontt@gmail.com>
2024-05-28 20:20:52 -0400

    creacion de homecontroller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,23 @@
 class HomeController < ApplicationController
   def index
-  @properties = Property.all
+    @properties = Property.all
+
+    if params[:min_price].present?
+      @properties = @properties.where('price >= ?', params[:min_price])
+    end
+
+    if params[:max_price].present?
+      @properties = @properties.where('price <= ?', params[:max_price])
+    end
+
+    if params[:bedrooms].present?
+      @properties = @properties.where('bedrooms >= ?', params[:bedrooms])
+    end
+
+    if params[:commune].present?
+      @properties = @properties.where(commune: params[:commune])
+    end
+
+    @properties = @properties.page(params[:page]).per(3)
   end
 end
